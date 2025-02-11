03b67917cc243a2c7026ad2d64e318fd5d82d165
zeiad-a7med <zeiadahmed194@gmail.com>
2025-02-11 23:12:10 +0200

    add ApiEndpoint class

---
+++ WeatherCast/API/ApiEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  ApeEndpoint.swift
+//  WeatherCast
+//
+//  Created by Zeiad on 11/02/2025.
+//
+
+import Foundation
+struct ApiEndpoint {
+    
+    static let baseUrl: String = "https://api.weatherapi.com/v1/forecast.json?key=d46ea35205b74e28bbd152315251102&q=29.975570, 31.105129&days=3&aqi=yes&alert=no"
+}
--- WeatherCast/Model/WeatherService/WeatherService.swift
+++ WeatherCast/Model/WeatherService/WeatherService.swift
@@ -19,7 +19,7 @@ class WeatherService: WeatherServiceProtocol {
         guard
             let url = URL(
                 string:
-                    "https://api.weatherapi.com/v1/forecast.json?key=d46ea35205b74e28bbd152315251102&q=29.975570, 31.105129&days=3&aqi=yes&alert=no"
+                    ApiEndpoint.baseUrl
             )
         else {
             print("Invalid URL")
